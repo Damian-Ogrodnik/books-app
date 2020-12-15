a01def9132e8d7b017efecfbd4309694d333bff6
Damian Ogrodnik <d.ogrodnik96@gmail.com>
2020-12-15 18:25:37 +0100

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   extends: ['airbnb-typescript-prettier'],
+//   rules: {
+//     'import/prefer-default-export': 'off',
+//     'arrow-body-style': ['warn', 'as-needed'],
+//   },
+//   settings: {
+//     'import/resolver': {
+//       typescript: {},
+//     },
+//   },
+//   env: {
+//     jest: true,
+//   },
+// };
